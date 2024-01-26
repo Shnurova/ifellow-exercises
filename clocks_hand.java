133852fc48b2dfc74f046def577a1e83b0a36ee7
Ludmila Shnurova <ludo4ka948@inbox.ru>
2024-01-26 22:31:32 +0300

    добавила проверку времени

--- clocks_hand.java
+++ clocks_hand.java
@@ -9,11 +9,27 @@ class ClocksHand
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите часы = "); 
         int hours = scanner.nextInt();
+        if (hours > 24 || hours < 0){
+            System.out.println("Ошибка в вводе часов ");
+            scanner.close();
+            return;
+        }
         if (hours >= 12)
             hours = hours - 12;
         System.out.println("Введите минуты = "); 
         int minutes = scanner.nextInt();
-        System.out.println("угол = " + ((Math.abs((minutes*6)-(hours*30))) - (minutes*0.5)));
+        if (minutes > 59 || minutes < 0){
+            System.out.println("Ошибка в вводе минут ");
+            scanner.close();
+            return;
+        }
+        double degree = (Math.abs((minutes*6)-(hours*30 + minutes*0.5)));
+        if (degree <= 180) {
+            System.out.println("угол = " + degree );
+        }
+        if (degree > 180){
+            System.out.println("угол = " + (360 - degree));
+        }
 
         scanner.close();
     }
